7ee3bb75fc3e1330871d51022d2394d98105cd64
xiaoxindada <2245062854@qq.com>
2021-10-07 10:35:15 +0000

    scripts: Switch to use apex_tools extract apexs

--- scripts/apex12_extractor.sh
+++ scripts/apex12_extractor.sh
@@ -1,39 +1,28 @@
 #!/bin/bash
 
 LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
-EXT4EXTRACT=$LOCALDIR/../tools/ext4extract/ext4extract.py
+APEXEXTRACT=$LOCALDIR/../tools/apex_tools/deapexer.py
 
 TMPDIR=$LOCALDIR/../tmp/apex_ext
 mkdir -p "$TMPDIR"
 
 APEXDIR="$1"
-APEXES=$(ls "$APEXDIR" | grep ".apex" | grep -v ".capex" )
+APEXES=$(ls "$APEXDIR" | grep ".*apex")
 for APEX in $APEXES; do
-    APEXNAME=$(echo "$APEX" | sed 's/.apex//')
+    APEXNAME=$(echo "$APEX" | sed 's/\.[^.]*$//')
     if [[ -d "$APEXDIR/$APEXNAME" ]]; then
         continue
     fi
+    if [[ $APEX == *".capex" ]]; then
+        7z e -y "$APEXDIR/$APEX" original_apex -o"$APEXDIR" 2>/dev/null >> "$TMPDIR"/zip.log
+        mv -f "$APEXDIR/original_apex" "$APEXDIR/$APEXNAME.apex"
+        rm -rf "$APEXDIR/$APEX"
+    fi
     mkdir -p "$APEXDIR/$APEXNAME"
-    7z e "$APEXDIR/$APEX" apex_payload.img apex_pubkey -o"$APEXDIR/$APEXNAME" 2>/dev/null >> "$TMPDIR"/zip.log
-    $EXT4EXTRACT "$APEXDIR/$APEXNAME/apex_payload.img" -D "$APEXDIR/$APEXNAME" 2>/dev/null
+    7z e -y "$APEXDIR/$APEX" apex_pubkey -o"$APEXDIR/$APEXNAME" 2>/dev/null >> "$TMPDIR"/zip.log
+    $APEXEXTRACT extract "$APEXDIR/$APEXNAME.apex" "$APEXDIR/$APEXNAME" 2>/dev/null
     rm "$APEXDIR/$APEXNAME/apex_payload.img"
     rm -rf "$APEXDIR/$APEXNAME/lost+found"
-done
-
-CAPEXES=$(ls "$APEXDIR" | grep ".capex" )
-for CAPEX in $CAPEXES; do
-   if [[ -z $CAPEX ]] ; then
-       continue
-   fi
-   CAPEXNAME=$(echo "$CAPEX" | sed 's/.capex//')
-   7z e -y "$APEXDIR/$CAPEX" original_apex -o"$APEXDIR" >> "$TMPDIR"/zip.log
-   rm -rf "$APEXDIR/$CAPEX"
-   mv -f "$APEXDIR/original_apex" "$APEXDIR/$CAPEXNAME.apex"
-   mkdir -p "$APEXDIR/$CAPEXNAME"
-   7z e -y "$APEXDIR/$CAPEXNAME.apex" apex_payload.img apex_pubkey -o"$APEXDIR/$CAPEXNAME" 2>/dev/null >> "$TMPDIR"/zip.log
-   $EXT4EXTRACT "$APEXDIR/$CAPEXNAME/apex_payload.img" -D "$APEXDIR/$CAPEXNAME" 2>/dev/null
-   rm -rf "$APEXDIR/$CAPEXNAME/apex_payload.img"
-   rm -rf "$APEXDIR/$CAPEXNAME/lost+found"
 done
 
 rm -rf "$TMPDIR"
