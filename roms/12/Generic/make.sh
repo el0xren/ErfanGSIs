2f790eee22cfb892e2da44462ae6ff8f3e8024e6
Nippon <37734266+nnippon@users.noreply.github.com>
2021-10-14 20:35:49 +0300

    12: update Generic make.sh

--- roms/12/Generic/make.sh
+++ roms/12/Generic/make.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-# booooo
+# Drop some services
+sed -i "/dataservice_app/d" $1/product/etc/selinux/product_seapp_contexts
+sed -i "/dataservice_app/d" $1/system_ext/etc/selinux/system_ext_seapp_contexts
+sed -i "/ro.sys.sdcardfs/d" $1/product/etc/build.prop
+
+# Some addons
+echo "ro.support_one_handed_mode=true" >> $1/build.prop
+echo "ro.boot.vendor.overlay.theme=com.google.android.systemui.gxoverlay" >> $1/product/etc/build.prop
